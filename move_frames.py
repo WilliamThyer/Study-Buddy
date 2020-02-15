2cc350d55b03eb1dcc7071f846114346d604638e
Colin Quirk <cquirk@uchicago.edu>
2020-02-15 17:17:00 -0600

    make valid and invalid folders

--- move_frames.py
+++ move_frames.py
@@ -1,18 +1,26 @@
+import random
 import os
 import shutil
 
+
 os.chdir('frames')
 
-if not os.path.isdir('attend'):
-    os.mkdir('attend')
+if not os.path.isdir('train'):
+    os.mkdir('train')
 
-if not os.path.isdir('nonattend'):
-    os.mkdir('nonattend')
+if not os.path.isdir('valid'):
+    os.mkdir('valid')
+
+os.mkdir('train/attend')
+os.mkdir('train/nonattend')
+os.mkdir('valid/attend')
+os.mkdir('valid/nonattend')
 
 for d in os.listdir('.'):
-    if os.path.isdir(d) and d not in ('attend', 'nonattend'):
+    if os.path.isdir(d) and d not in ('train', 'valid'):
+        dataset = 'valid' if random.random() <= 0.3 else 'train'
         folder = 'nonattend' if int(d) % 2 == 0 else 'attend'
 
         for f in os.listdir(d):
             if not f.startswith('.DS'):
-                shutil.copy(os.path.join(d, f), os.path.join(folder, f'{d}_{f}'))
+                shutil.copy(os.path.join(d, f), os.path.join(dataset, folder, f'{d}_{f}'))
